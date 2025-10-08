6e7df108a2c46ac8ba1234a4333546a35acaa565
YousraFarhani <jy_farhani@esi.dz>
2025-10-08 16:00:10 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use absolute root path for production hosting
+  base: './', // Use relative paths for Netlify deployment
 })
 
